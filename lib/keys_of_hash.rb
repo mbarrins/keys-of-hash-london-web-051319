f95cc52f1c81be10f27d7581aec4e4edf1afe9f5
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 15:55:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,13 +3,14 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # binding.pry
-    keys_of_args = []
-    arguments.each do |argument|
-      puts arguments.inspect
-      puts argument
-      self.select{|key, value| keys_of_args << key if value == argument}
-    end
-    keys_of_args
+    # keys_of_args = []
+    # arguments.each do |argument|
+    #   puts arguments.inspect
+    #   puts argument
+    #   self.select{|key, value| keys_of_args << key if value == argument}
+    # end
+    # keys_of_args
+    self.select{|key, value| keys_of_args << key if arguments.include?(value)}.keys
   end
 end
 
