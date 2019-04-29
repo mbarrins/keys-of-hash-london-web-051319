dbfcdde0f3345e73669d71f09d2f5f0435fe18c9
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 15:24:23 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    self.select
   end
 end
 
-animals = { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
 puts animals.keys_of("Australia").inspect
