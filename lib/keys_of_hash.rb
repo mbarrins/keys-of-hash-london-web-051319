50c7cdc8aceba3ed652b978733d0da1e4ca2f9e1
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 15:56:49 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,18 +2,6 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    # binding.pry
-    # keys_of_args = []
-    # arguments.each do |argument|
-    #   puts arguments.inspect
-    #   puts argument
-    #   self.select{|key, value| keys_of_args << key if value == argument}
-    # end
-    # keys_of_args
     self.select{|key, value| arguments.include?(value)}.keys
   end
 end
-
-animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-puts animals.keys_of("Australia").inspect
-puts animals.keys_of('Australia', 'Panama').inspect
