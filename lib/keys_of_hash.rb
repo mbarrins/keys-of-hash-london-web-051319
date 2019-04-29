75510a0195f9c9721e1937e709f93c0bcf236d4c
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 15:48:09 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -12,5 +12,4 @@ end
 
 animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
 # puts animals.keys_of("Australia").inspect
-puts
 puts animals.keys_of('Australia', 'Panama')
